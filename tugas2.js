569a8ac5dffc747235382dc191367c0fc1fc2ef8
Fiqrian <fiqrian.faturahman@gmail.com>
2021-11-20 20:30:17 +0700

    tugas2 javascript

---
+++ tugas2.js
@@ -0,0 +1,15 @@
+var number = 5;
+console.log(number);
+var string = "number";
+console.log(string);
+var bool = 180;
+
+if (bool > 170) bool = true;
+else bool = false;
+console.log(bool);
+
+var nul = null;
+var und;
+console.log(nul, und);
+var arr = ["Fiqrian", "Faturachman", 20];
+console.log(arr);
